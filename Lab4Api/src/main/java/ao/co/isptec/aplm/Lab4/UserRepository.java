a88032ee0d31d316c1b6ab2a1839402292f283f0
WilkerMatias <164266994+WilkerMatias@users.noreply.github.com>
2024-11-01 14:10:51 +0100

    Delete Lab4Api directory

--- Lab4Api/src/main/java/ao/co/isptec/aplm/Lab4/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package ao.co.isptec.aplm.Lab4;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
